fd61412597d64c1d0e1d3c8f7630fa67dd375097
ankang.gao@imperial.ac.uk <agao1@ic.ac.uk>
2021-10-26 19:49:01 +0100

    update

--- processforcepluning0p5.m
+++ processforcepluning0p5.m
@@ -1,27 +1,27 @@
 %%
 % clear;
 %% parameters
-k=3.5;
-forcefilename='force.dat';
+k=1.2;
 Amp = 0.5;
-%%
+forcefilename='force.dat';
+%% dependent parameters
 Fref = 0.5;
 Stime = 0;
 Tper = pi/k;
 mode = 1;
 StA = k*Amp/pi
-%%
+%% load data
 % clc;
 close all;
 setPlotParameters;
 nskip = 5;
 nvars = 7;
 file = loadequispacedtimeseries(forcefilename, nskip, nvars);
-%%
-phaseIndex = getfileDividers(file, Tper, 4);
 t = file.data(:, 1);
 fx = file.data(:,4);
 fy = file.data(:,7);
+phaseIndex = getfileDividers(file, Tper, 4);
+%% calculate added mass force
 amp = 0.5 * Amp;
 omega = 2 * k;
 ay = -omega*omega*amp*cos(omega*t);
@@ -39,30 +39,12 @@ figure
 plot(t/Tper, fy/Fref, 'b-')
 hold on
 plot(t/Tper, Faddedy/Fref, 'r-')
-plot(t(phaseIndex{1})/Tper, Faddedy(phaseIndex{1})/Fref, '+')
-plot(t(phaseIndex{2})/Tper, Faddedy(phaseIndex{2})/Fref, 'v')
-plot(t(phaseIndex{3})/Tper, Faddedy(phaseIndex{3})/Fref, 'x')
-plot(t(phaseIndex{4})/Tper, Faddedy(phaseIndex{4})/Fref, '^')
 xlabel('t/T')
 ylabel('C_L')
 legend('total', 'added mass', 'Location', 'Best')
+%% substract added mass force from the total force
 fxvor = fx - Faddedx;
 fyvor = fy - Faddedy;
-%%
-figure
-plot(t/Tper, file.data(:,4)/Fref, 'b-')
-xlabel('t/T')
-ylabel('C_D - added mass force')
-figure
-plot(t/Tper, file.data(:,7)/Fref, 'b-')
-xlabel('t/T')
-ylabel('C_L - added mass force')
-%%
-figure;
-plot(Faddedx/Fref, Faddedy/Fref, 'r')
-hold on
-plot(fx/Fref, fy/Fref, 'b')
-figure;
 CX = fxvor / Fref;
 CY = fyvor / Fref;
 cxmax = max(CX);
@@ -71,6 +53,21 @@ cymax = max(CY);
 cymin = min(CY);
 cxabsmax = max(cxmax, -cxmin);
 cyabsmax = max(cymax, -cymin);
+%% plot vortex force
+figure
+plot(t/Tper, file.data(:,4)/Fref, 'b-')
+xlabel('t/T')
+ylabel('C_D - added mass force')
+figure
+plot(t/Tper, file.data(:,7)/Fref, 'b-')
+xlabel('t/T')
+ylabel('C_L - added mass force')
+%% CD-CL phase plot
+figure;
+plot(Faddedx/Fref, Faddedy/Fref, 'r')
+hold on
+plot(fx/Fref, fy/Fref, 'b')
+figure;
 plot(CX, CY, 'b-')
 hold on;
 plot(CX(phaseIndex{1}), CY(phaseIndex{1}), '+m')
@@ -81,14 +78,15 @@ xlabel('C_D')
 ylabel('C_L')
 axis([-cxabsmax, cxabsmax, -cyabsmax, cyabsmax])
 saveas(gcf, strcat(num2str(k), 'CL_CD.png'))
-%%
+%% statistic data, can only run ontime
 file.data(:,4) = fxvor;
 file.data(:,7) = fyvor;
-[periodicity4, dominantfrequency4, meanv4, sigmamean4, maxv, minv, dominantamp] = showp(forcefilename, 4, Stime, file, Tper, mode)
+[periodicity4, dominantfrequency4, meanv4, sigmamean4, maxv4, minv4, dominantamp] = showp(forcefilename, 4, Stime, file, Tper, mode)
 dragforce=[periodicity4, meanv4]
-[periodicity7, dominantfrequency7, meanv7, sigmamean7, maxv, minv, dominantamp] = showp(forcefilename, 7, Stime, file, Tper, mode)
+[periodicity7, dominantfrequency7, meanv7, sigmamean7, maxv7, minv7, dominantamp] = showp(forcefilename, 7, Stime, file, Tper, mode)
 liftforce=[periodicity7, meanv7]
-result = [meanv4/Fref, sigmamean4/Fref, meanv7/Fref, sigmamean7/Fref,...
+result = [k Amp meanv4/Fref, sigmamean4/Fref, maxv4/Fref, minv4/Fref,...
+                meanv7/Fref, sigmamean7/Fref, maxv7/Fref, minv7/Fref,...
     0.5*(periodicity4 + periodicity7), 0.5*(dominantfrequency4 + dominantfrequency7), ...
     cxmin, cxmax, cymin, cymax]
 %%
