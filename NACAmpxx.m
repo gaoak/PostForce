9d283b5ca7d8a34293f2dc9b5a80d63cf1f98b98
Ankang Gao <ankanggao@ustc.edu.cn>
2024-06-26 10:36:21 +0800

    add nacafoil

--- NACAmpxx.m
+++ NACAmpxx.m
@@ -9,8 +9,20 @@ for i=1:1:5
     xs(:, i+1) = xs(:, i).*x;
 end
 y = 5.*mt*(0.2969*sqrt(x) -0.1260*xs(:,1) - 0.3516*xs(:,2) + 0.2843*xs(:,3) - 0.1015*xs(:,4));
+sy= 5.*mt*(0.2969*2/3*sqrt(x).*x -0.1260/2*xs(:,2) - 0.3516/3*xs(:,3) + 0.2843/4*xs(:,4) - 0.1015/5*xs(:,5));
+rad0 = (5.*mt*0.2969)^2 / 2;
+y0 = sqrt(abs(2 * rad0 * x - x.*x));
+y1 = sqrt(abs(2 * rad0 * x ));
+rad1 = rad0 * 1.5;
+y2 = sqrt(abs(2 * rad1 * x - x.*x));
 index = [len:-1:1 2:1:len];
 points = [x(index) [y(len:-1:1);-y(2:1:len)] index'*0];
 figure;
 plot(points(:,1), points(:,2), '.-')
+hold on
+plot(x, y0, '-r')
+plot(x, y1, '-k')
+plot(x, y2, '-g')
+axis([0 1 -0.5 0.5])
+pbaspect([1 1 1])
 save('naca0012.dat', 'points', '-ascii', '-double')
